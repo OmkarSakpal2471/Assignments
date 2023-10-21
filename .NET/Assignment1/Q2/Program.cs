8ab34dd706835d872c3129a5ddef0f92a0bf4f7a
OmkarSakpal2471 <90462472+OmkarSakpal2471@users.noreply.github.com>
2023-10-21 17:29:02 +0530

    Add files via upload

---
+++ .NET/Assignment1/Q2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1=new Class1();
+            int result=c1.fact(4);
+            Console.WriteLine(result);
+        }
+    }
+}
